123e9ad3b805d1972b8ede7b4cd11e0b6a78b80a
junepaykim <49562343+junepaykim@users.noreply.github.com>
2022-10-16 21:41:56 +0900

    2022.10.16

---
+++ Chapter3/pandastutorial.py
@@ -0,0 +1,22 @@
+'''initial pandas usage'''
+import pandas as pd
+import numpy as np
+
+population_dict = {'California' : 38332521,
+                   'Texas' : 26448193,
+                   'New York' : 19651127,
+                   'Florida' : 19552860,
+                   'Illinois' : 12882135}
+area_dict = {'California' : 1,
+                   'Texas' : 2,
+                   'New York' : 33333,
+                   'Florida' : 44,
+                   'Illinois' : 555}
+population = pd.Series(population_dict)
+pd.DataFrame(population, columns=['population'])
+pd.DataFrame(population, columns=['test1'])
+
+df = pd.DataFrame([[1, np.nan, 2],
+                   [7, 3, 5],
+                   [np.nan, 4, 6]])
+df
