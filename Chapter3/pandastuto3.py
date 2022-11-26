2b29861b009dfd1f2a33adb11eeb53e5af4dc103
junepaykim <49562343+junepaykim@users.noreply.github.com>
2022-11-26 21:17:50 +0900

    22.11.26 study

--- Chapter3/pandastuto3.py
+++ Chapter3/pandastuto3.py
@@ -5,14 +5,12 @@ import pandas as pd
 def main() :
     '''pandas3 main'''
     print(make_df('ABC', range(3)))
-    
+
+if __name__ == "__main__" :
+    main()
 
 def make_df(cols, ind):
     """Quickly make a DataFrame"""
     data = {c : [str(c) + str(i) for i in ind]
             for c in cols}
     return pd.DataFrame(data, ind)
-
-if __name__ == "__main__" :
-    main()
-    
