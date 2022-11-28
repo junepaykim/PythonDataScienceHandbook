5006122f4a3a65ff05a650b2991d2a5ad4016bd6
junepaykim <49562343+junepaykim@users.noreply.github.com>
2022-11-28 21:22:54 +0900

    22.11.28 night

---
+++ Chapter3/aggregationandgrouping.py
@@ -0,0 +1,16 @@
+'''2022.11.28 study'''
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+def main() :
+    '''introduction to seaborn'''
+    planets = sns.load_dataset('planets')    
+    #print(planets.shape)
+    
+    print(planets.dropna().shape)
+    print(planets.dropna().describe())
+    
+if __name__ == "__main__" :
+    main()
+    
