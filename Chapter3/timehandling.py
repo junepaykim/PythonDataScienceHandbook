4834713664b1175d8229d3a3e9698e6d659119cc
junepaykim <49562343+junepaykim@users.noreply.github.com>
2022-12-22 20:57:56 +0900

    2022.12.22

--- Chapter3/timehandling.py
+++ Chapter3/timehandling.py
@@ -4,6 +4,11 @@ import pandas as pd
 import numpy as np
 from dateutil import parser
 import numpy as np
+from pandas_datareader import data as pdr
+import matplotlib.pyplot as plt
+import seaborn
+import yfinance as yf
+
 
 
 def pythonNative() :
@@ -46,13 +51,71 @@ def pandasTime() :
     from pandas.tseries.offsets import BDay
     pd.date_range('2015-07-01', periods=5, freq=BDay())
     
+def pandasTimeExample():
+    '''pandas time example function'''
+    '''Google chart is shown in book for example but google does not provide chart any more'''
+    '''So, use yahoo chat. install pip install yfinance first'''
+    yf.pdr_override()
+    data = pdr.get_data_yahoo('005930.KS', start='2012-01-01', end='2020-12-31')
+    #print(data.head())
+    data = data['Close']
+    
+    seaborn.set()  
+    data.plot()    
+    
+    #print(type(data.resample('BA').mean()))
+    #print(type(data.asfreq('BA')))
+    data.plot(alpha=0.5, style='-')
+    data.resample('BA').mean().plot(style=':')
+    data.asfreq('BA').plot(style='--')
+    plt.legend(['input', 'resample', 'asfreq'], loc='upper left')
+    '''input does not work for some reason...'''
+    
+    '''Working for NA values // forward fill vs backward fill'''
+    fig, ax = plt.subplots(2, sharex=True)
+    yaho = data.iloc[:10]
+    yaho.asfreq('D').plot(ax=ax[0], marker='o')
+    yaho.asfreq('D', method='bfill').plot(ax=ax[1], style='-o')
+    yaho.asfreq('D', method='ffill').plot(ax=ax[1], style='--o')
+    ax[1].legend(["back-fill", "forward-fill"])
+    
+    '''Time shift'''
+    fig, ax = plt.subplots(3, sharey=True)
+    yaho = data.asfreq('D', method='pad')
+    
+    yaho.plot(ax=ax[0])
+    yaho.shift(900).plot(ax=ax[1])
+    yaho.tshift(900).plot(ax=ax[2])
+    
+    local_max = pd.to_datetime('2017-11-05')
+    offset = pd.Timedelta(900, 'D')
+    
+    ax[0].legend(['input'], loc=2)
+    ax[0].get_xticklabels()[1].set(weight='heavy', color='red')
+    ax[0].axvline(local_max, alpha=0.3, color='red')
+    
+    ax[1].legend(['shift(900)'], loc=2)
+    ax[1].get_xticklabels()[1].set(weight='heavy', color='red')
+    ax[1].axvline(local_max + offset, alpha=0.3, color='red')
+    
+    ax[2].legend(['tshift(900)'], loc=2)
+    ax[2].get_xticklabels()[1].set(weight='heavy', color='red')
+    ax[2].axvline(local_max + offset, alpha=0.3, color='red')
+    
+    
+    '''value compute'''
+    ROI = 100 * (yaho.tshift(-365) / yaho - 1)
+    yaho.plot()
+    plt.ylabel('% Return on Investment')
+    
     
 
 def main() :
     '''time handling main'''
-    pythonNative()
+    """ pythonNative()
     numpyTime()
-    pandasTime()
+    pandasTime() """
+    pandasTimeExample()
     
 
 if __name__ == "__main__" :
