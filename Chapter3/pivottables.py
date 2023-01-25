d4d05ae4e7638a8f08de1b2aaad4f49cbae0d610
junepaykim <49562343+junepaykim@users.noreply.github.com>
2023-01-25 21:11:56 +0900

    custome legend

--- Chapter3/pivottables.py
+++ Chapter3/pivottables.py
@@ -1,14 +1,20 @@
-'''pivot table study'''
+"""pivot table study"""
 import pandas as pd
 import numpy as np
 import seaborn as sns
 
-def main() :
-    '''pivot table main'''
-    titanic = sns.load_dataset('titanic')
-    print(titanic.groupby(['sex','class'])['survived'].aggregate('mean').unstack())
-    print(titanic.groupby(['sex','class'])['survived'].mean())
-    
 
-if __name__ == "__main__" :
+def main():
+    """pivot table main"""
+    titanic = sns.load_dataset("titanic")
+    print(
+        titanic.groupby(["sex", "class"])["survived"]
+        .aggregate("mean")
+        .unstack()
+    )
+    print(titanic.groupby(["sex", "class"])["survived"].mean())
+
+
+if __name__ == "__main__":
     main()
+    print("Hello World!")
